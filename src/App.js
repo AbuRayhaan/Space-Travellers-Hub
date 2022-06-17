4eea36a0d30688b7b96caa6fa593139ac5f3fc6c
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-06-17 20:49:09 +0100

    Modified the App.js file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { FetchRockets } from './redux/rocket/rocketSlice'
 import { useDispatch } from 'react-redux';
+import { FetchRockets } from './redux/rocket/rocketSlice';
 import Navbar from './components/rocket/Navbar';
 import Rocket from './pages/Rocket';
 import Mission from './pages/Mission';
 import MyProfile from './pages/Profile';
-import Footer from './components/Footer'
+import Footer from './components/Footer';
 
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(FetchRockets())
-  }, [])
+    dispatch(FetchRockets());
+  }, []);
 
   return (
     <>
